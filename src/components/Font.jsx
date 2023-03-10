72a1390827ea02ebf04b85aaa890b1ccf58c34cc
kartikey48 <ranakartik48@gmail.com>
2023-03-10 14:16:22 +0530

    Deployed website

---
+++ src/components/Font.jsx
@@ -0,0 +1,11 @@
+
+function Font(){
+    return(
+        <div className="head">
+            <span><h1>KHOJANALYA</h1>
+            <h2>YOUR MULTIPLE STOP FOR FOOD</h2></span> 
+        </div>
+    )
+}
+
+export default Font ;
---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,31 @@
+import React,{useState} from 'react'
+import { FaSearch } from 'react-icons/fa'
+import "./SearchBar.css";
+
+
+export const SearchBar = ({setResults}) => {
+const [input, setInput] = useState("")
+
+
+const fetchData = (value) =>{
+    fetch("https://jsonplaceholder.typicode.com/users").then((response) => response.jason()).then((json) =>{
+        const results = json.filter((user) => {
+            return value && user && user.name && user.name.toLowerCase().includes(value)
+        })
+        setResults(results);
+    })
+
+}
+
+const handelChange = (value) =>{
+    setInput(value)
+    fetchData(value)
+}
+  return (
+    <div className='input-wrapper'>
+        <FaSearch id="search-icon" />
+        <input placeholder='type to search' value={input} 
+        onChange={(e) => handelChange(e.target.value)} />
+    </div>
+  )
+}
