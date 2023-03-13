5d996e162af34e46b5178c96e9900c32d477fb0b
AyushChamoli961 <ayushchamoli001@gmail.com>
2023-03-13 12:39:10 +0530

    footer added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React,{useState} from 'react';
 import Home from './components/Home/Home';
 import Dishes from './components/Dishes/Dishes';
 import Resturants from './components/Resturants/Resturants';
+import Footer from './components/Footer/Footer';
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
     <Home/>
     <Dishes/>
     <Resturants/>
+    <Footer/>
     </>
 )  
 }
--- src/components/Resturants/Resturants.jsx
+++ src/components/Resturants/Resturants.jsx
@@ -27,6 +27,34 @@ const Resturants = () => {
         image="https://images.pexels.com/photos/1639556/pexels-photo-1639556.jpeg?auto=compress&cs=tinysrgb&w=800"
         name= "Rana ka Dhaba"
         />
+        <Resturant 
+        image="https://images.pexels.com/photos/793765/pexels-photo-793765.jpeg?auto=compress&cs=tinysrgb&w=800"
+        name= "Rana ka Dhaba"
+        />
+        <Resturant 
+        image="https://images.pexels.com/photos/1639556/pexels-photo-1639556.jpeg?auto=compress&cs=tinysrgb&w=800"
+        name= "Rana ka Dhaba"
+        />
+        <Resturant 
+        image="https://images.pexels.com/photos/3659862/pexels-photo-3659862.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+        name= "Rana ka Dhaba"
+        />
+        <Resturant 
+        image="https://images.pexels.com/photos/1633525/pexels-photo-1633525.jpeg?auto=compress&cs=tinysrgb&w=800"
+        name= "Rana ka Dhaba"
+        />
+        <Resturant 
+        image="https://images.pexels.com/photos/1639556/pexels-photo-1639556.jpeg?auto=compress&cs=tinysrgb&w=800"
+        name= "Rana ka Dhaba"
+        />
+        <Resturant 
+        image="https://images.pexels.com/photos/1633525/pexels-photo-1633525.jpeg?auto=compress&cs=tinysrgb&w=800"
+        name= "Rana ka Dhaba"
+        />
+        <Resturant 
+        image="https://images.pexels.com/photos/1639556/pexels-photo-1639556.jpeg?auto=compress&cs=tinysrgb&w=800"
+        name= "Rana ka Dhaba"
+        />
         </div>
         
     </div>
